1c280fd7d8d9ca05b881ef7cda18710bb86ac9f7
Clément Aigreault <6172122+clementai@users.noreply.github.com>
2025-01-13 11:32:48 +0100

    Always install gum during install

--- install.sh
+++ install.sh
@@ -7,13 +7,14 @@ RUNNING_GNOME=$([[ "$XDG_CURRENT_DESKTOP" == *"GNOME"* ]] && echo true || echo f
 # Check the distribution name and version and abort if incompatible
 source ~/.local/share/omakub/install/check-version.sh
 
+source ~/.local/share/omakub/install/terminal/required/app-gum.sh >/dev/null
+
 if $RUNNING_GNOME; then
   # Ensure computer doesn't go to sleep or lock while installing
   gsettings set org.gnome.desktop.screensaver lock-enabled false
   gsettings set org.gnome.desktop.session idle-delay 0
 
   echo "Get ready to make a few choices..."
-  source ~/.local/share/omakub/install/terminal/required/app-gum.sh >/dev/null
   source ~/.local/share/omakub/install/first-run-choices.sh
 
   echo "Installing terminal and desktop tools..."
